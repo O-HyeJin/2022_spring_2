afe7d54bfbc68b6b4595c906b51c3e1c961df8f5
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2022-02-28 12:16:48 +0900

    Chore: home 화면에서 영화 리스트가 출력되게끔 home 메소드 수정

--- src/main/java/com/comento/spring/HomeController.java
+++ src/main/java/com/comento/spring/HomeController.java
@@ -2,7 +2,10 @@ package com.comento.spring;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.comento.spring.movie.MovieVO;
+import com.comento.spring.movie.service.MovieService;
 
 /**
  * Handles requests for the application home page.
@@ -19,19 +25,20 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Inject
+	private MovieService service;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * @throws Exception 
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Locale locale, Model model) throws Exception {
+		logger.info("home");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		List<MovieVO> movieList = service.selectMovie();
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("movieList", movieList);
 		
 		return "home";
 	}
